14ff2363d4840910cf510fb2b97099c3bd6010a5
IvelizAyala <ayalaiveliz@gmail.com>
2021-11-26 18:15:47 -0400

    feat:Create pagePrincipal

---
+++ my-app/public/Principal.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import "../css/Repositorio.css";
+//import ButtonsOfRepo from "../components/ButtonsOfRepo";
+//import HeaderMainPage from "../components/HeaderRepositorio";
+//import FooterRepositorio from "../components/FooterRepositorio";
+
+function Repositorio() {
+  return (
+   <>
+    
+    </>
+  );
+}
+export default Repositorio;
